b1ecad5af0bd8a25e6cb126a94354fc2e99717bc
zaricu22 <48631871+zaricu22@users.noreply.github.com>
2024-02-09 15:17:29 +0100

    empty SpringBoot project

---
+++ src/main/java/com/example/pharmacyback/PharmacyBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pharmacyback/PharmacyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
