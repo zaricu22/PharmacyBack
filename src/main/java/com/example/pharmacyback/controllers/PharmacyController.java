17b345e6ebd4215ff3a3716cd24e2c79828a5974
zaricu22 <48631871+zaricu22@users.noreply.github.com>
2024-02-13 15:14:19 +0100

    solve cross origin

--- src/main/java/com/example/pharmacyback/controllers/PharmacyController.java
+++ src/main/java/com/example/pharmacyback/controllers/PharmacyController.java
@@ -5,12 +5,14 @@ import com.example.pharmacyback.crud.ProductRepository;
 import com.example.pharmacyback.model.Product;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class PharmacyController {
     @Autowired
